335d256682c9640f7ee59afc3f6e6a87337247ac
clearchaos3 <clearchaos3@gmail.com>
2022-06-13 01:06:54 -0500

    added more filter, sorting, and sticky header

--- src/Guitars.js
+++ src/Guitars.js
@@ -7,51 +7,215 @@ export const Guitars = () => {
     const [color, setColor] = useState('');
     const [minPrice, setMinPrice] = useState('');
     const [maxPrice, setMaxPrice] = useState('');
-    const [sortOrder, setSortOrder] = useState('sortPriceAscending')
+    const [sortOrder, setSortOrder] = useState('priceAscending')
 
-    function sortFunction(a, b, sortOrder) {
-        console.log("sortOrder inside function:", sortOrder)
-        if (sortOrder === 'sortPriceAscending') {
-            let keyA = parseInt(a.price.replace(/[$,]+/g, ""))
-            let keyB = parseInt(b.price.replace(/[$,]+/g, ""))
-            if (keyA < keyB) return -1;
-            if (keyA > keyB) return 1;
-            return 0;
-        }
+    function sortPriceAscending(a, b) {
+        let keyA = parseInt(a.price.replace(/[$,]+/g, ""))
+        let keyB = parseInt(b.price.replace(/[$,]+/g, ""))
+        if (keyA < keyB) return -1;
+        if (keyA > keyB) return 1;
+        return 0;
+    }
+    function sortPriceDescending(a, b) {
+        let keyA = parseInt(a.price.replace(/[$,]+/g, ""))
+        let keyB = parseInt(b.price.replace(/[$,]+/g, ""))
+        if (keyA > keyB) return -1;
+        if (keyA < keyB) return 1;
+        return 0;
+    }
+    function sortModelAscending(a, b) {
+        let keyA = a.model
+        let keyB = b.model
+        if (keyA < keyB) return -1;
+        if (keyA > keyB) return 1;
+        return 0;
+    }
+    function sortModelDescending(a, b) {
+        let keyA = a.model
+        let keyB = b.model
+        if (keyA > keyB) return -1;
+        if (keyA < keyB) return 1;
+        return 0;
     }
 
-    console.log("sortOrder:", sortOrder)
+    if (sortOrder === 'priceAscending') {
+        return (
+            <div>
+                <div className="header">
+                    <div className="title"><img src="https://images.ctfassets.net/m8onsx4mm13s/72p8qgeqA4UK7FD2MwyA19/cbc4a71ff92b788b791a53fa683dc6f4/gibson__1_.svg" alt="gibson logo"></img>Modded Mod Shop</div>
 
-    return (
-        <div>
-            <div className="guitar-container">
-                <div>
-                    <input id="modelFilter" placeholder="Model" name="filter" type="text" value={model} onChange={event => setModel(event.target.value)}></input>
-                    <input id="colorFilter" placeholder="Color" name="filter" type="text" value={color} onChange={event => setColor(event.target.value)}></input>
-                    <input id="minPriceFilter" placeholder="Minimum Price" name="filter" type="text" value={minPrice} onChange={event => setMinPrice(event.target.value)}></input>
-                    <input id="maxPriceFilter" placeholder="Maximum Price" name="filter" type="text" value={maxPrice} onChange={event => setMaxPrice(event.target.value)}></input>
+                    <div className="centered">
+                        <input id="modelFilter" placeholder="Model" name="filter" type="text" value={model} onChange={event => setModel(event.target.value)}></input>
+                        <input id="colorFilter" placeholder="Color" name="filter" type="text" value={color} onChange={event => setColor(event.target.value)}></input>
+                        <input id="minPriceFilter" placeholder="Minimum Price" name="filter" type="text" value={minPrice} onChange={event => setMinPrice(event.target.value)}></input>
+                        <input id="maxPriceFilter" placeholder="Maximum Price" name="filter" type="text" value={maxPrice} onChange={event => setMaxPrice(event.target.value)}></input>
+                    </div>
+                    <div className="centered">
+                        <button className="sortButton activeButton" onClick={() => setSortOrder('priceAscending')}><span role="img" aria-label="Money Bag">💰</span><span role="img" aria-label="Index Pointing Up">👆</span></button>
+                        <button className="sortButton" onClick={() => setSortOrder('priceDescending')}><span role="img" aria-label="Money Bag">💰</span><span role="img" aria-label="Index Pointing Down">👇</span></button>
+                        <button className="sortButton" onClick={() => setSortOrder('modelAscending')}><span role="img" aria-label="Guitar">🎸</span><span role="img" aria-label="Index Pointing Up">👆</span></button>
+                        <button className="sortButton" onClick={() => setSortOrder('modelDescending')}><span role="img" aria-label="Guitar">🎸</span><span role="img" aria-label="Index Pointing Down">👇</span></button>
+                    </div>
                 </div>
-                <div><button onClick={() => setSortOrder('sortPriceAscending')}>Price <span role="img" aria-label="Index Pointing Up">👆</span></button></div>
-                {guitarData.sort(sortFunction).filter(f => (
-                    (f.model.toLowerCase().includes(model.toLowerCase()) || model === '')
-                    && (f.color.toLowerCase().includes(color.toLowerCase()) || color === '')
-                    && (parseFloat(f.price.replace(/[$,]+/g, "")) > minPrice || minPrice === '')
-                    && (parseFloat(f.price.replace(/[$,]+/g, "")) < maxPrice || maxPrice === '')
-                )
-                )
-                    .map((data, key) => {
-                        return (
-                            <div key={key} className="card">
-                                <img src={'./resizedImages' + data.image.substring(data.image.indexOf('/RAM'), data.image.length - 3) + 'jpg'} alt="guitar" />
-                                <div className="cardText">
-                                    <div className="model">{data.model}</div>
-                                    <div className="color">{data.color}</div>
-                                    <div className="price">{data.price}</div>
-                                </div>
-                            </div>
-                        );
-                    })}
+                <div className="guitar-container">
+                    <div className="cards">
+                        {guitarData.sort(sortPriceAscending).filter(f => (
+                            (f.model.toLowerCase().includes(model.toLowerCase()) || model === '')
+                            && (f.color.toLowerCase().includes(color.toLowerCase()) || color === '')
+                            && (parseFloat(f.price.replace(/[$,]+/g, "")) > minPrice || minPrice === '')
+                            && (parseFloat(f.price.replace(/[$,]+/g, "")) < maxPrice || maxPrice === '')
+                        )
+                        )
+                            .map((data, key) => {
+                                return (
+                                    <div key={key} className="card">
+                                        <img src={'./resizedImages' + data.image.substring(data.image.indexOf('/RAM'), data.image.length - 3) + 'jpg'} alt="guitar" />
+                                        <div className="cardText">
+                                            <div className="model">{data.model}</div>
+                                            <div className="color">{data.color}</div>
+                                            <div className="price">{data.price}</div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else if (sortOrder === 'priceDescending') {
+        return (
+            <div>
+                <div className="header">
+                    <div className="title"><img src="https://images.ctfassets.net/m8onsx4mm13s/72p8qgeqA4UK7FD2MwyA19/cbc4a71ff92b788b791a53fa683dc6f4/gibson__1_.svg" alt="gibson logo"></img>Modded Mod Shop</div>
+
+                    <div className="centered">
+                        <input id="modelFilter" placeholder="Model" name="filter" type="text" value={model} onChange={event => setModel(event.target.value)}></input>
+                        <input id="colorFilter" placeholder="Color" name="filter" type="text" value={color} onChange={event => setColor(event.target.value)}></input>
+                        <input id="minPriceFilter" placeholder="Minimum Price" name="filter" type="text" value={minPrice} onChange={event => setMinPrice(event.target.value)}></input>
+                        <input id="maxPriceFilter" placeholder="Maximum Price" name="filter" type="text" value={maxPrice} onChange={event => setMaxPrice(event.target.value)}></input>
+                    </div>
+                    <div className="centered">
+                        <button className="sortButton" onClick={() => setSortOrder('priceAscending')}><span role="img" aria-label="Money Bag">💰</span><span role="img" aria-label="Index Pointing Up">👆</span></button>
+                        <button className="sortButton activeButton" onClick={() => setSortOrder('priceDescending')}><span role="img" aria-label="Money Bag">💰</span><span role="img" aria-label="Index Pointing Down">👇</span></button>
+                        <button className="sortButton" onClick={() => setSortOrder('modelAscending')}><span role="img" aria-label="Guitar">🎸</span><span role="img" aria-label="Index Pointing Up">👆</span></button>
+                        <button className="sortButton" onClick={() => setSortOrder('modelDescending')}><span role="img" aria-label="Guitar">🎸</span><span role="img" aria-label="Index Pointing Down">👇</span></button>
+                    </div>
+                </div>
+                <div className="guitar-container">
+                    <div className="cards">
+                        {guitarData.sort(sortPriceDescending).filter(f => (
+                            (f.model.toLowerCase().includes(model.toLowerCase()) || model === '')
+                            && (f.color.toLowerCase().includes(color.toLowerCase()) || color === '')
+                            && (parseFloat(f.price.replace(/[$,]+/g, "")) > minPrice || minPrice === '')
+                            && (parseFloat(f.price.replace(/[$,]+/g, "")) < maxPrice || maxPrice === '')
+                        )
+                        )
+                            .map((data, key) => {
+                                return (
+                                    <div key={key} className="card">
+                                        <img src={'./resizedImages' + data.image.substring(data.image.indexOf('/RAM'), data.image.length - 3) + 'jpg'} alt="guitar" />
+                                        <div className="cardText">
+                                            <div className="model">{data.model}</div>
+                                            <div className="color">{data.color}</div>
+                                            <div className="price">{data.price}</div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    else if (sortOrder === 'modelAscending') {
+        return (
+            <div>
+                <div className="header">
+                    <div className="title"><img src="https://images.ctfassets.net/m8onsx4mm13s/72p8qgeqA4UK7FD2MwyA19/cbc4a71ff92b788b791a53fa683dc6f4/gibson__1_.svg" alt="gibson logo"></img>Modded Mod Shop</div>
+
+                    <div className="centered">
+                        <input id="modelFilter" placeholder="Model" name="filter" type="text" value={model} onChange={event => setModel(event.target.value)}></input>
+                        <input id="colorFilter" placeholder="Color" name="filter" type="text" value={color} onChange={event => setColor(event.target.value)}></input>
+                        <input id="minPriceFilter" placeholder="Minimum Price" name="filter" type="text" value={minPrice} onChange={event => setMinPrice(event.target.value)}></input>
+                        <input id="maxPriceFilter" placeholder="Maximum Price" name="filter" type="text" value={maxPrice} onChange={event => setMaxPrice(event.target.value)}></input>
+                    </div>
+                    <div className="centered">
+                        <button className="sortButton" onClick={() => setSortOrder('priceAscending')}><span role="img" aria-label="Money Bag">💰</span><span role="img" aria-label="Index Pointing Up">👆</span></button>
+                        <button className="sortButton" onClick={() => setSortOrder('priceDescending')}><span role="img" aria-label="Money Bag">💰</span><span role="img" aria-label="Index Pointing Down">👇</span></button>
+                        <button className="sortButton activeButton" onClick={() => setSortOrder('modelAscending')}><span role="img" aria-label="Guitar">🎸</span><span role="img" aria-label="Index Pointing Up">👆</span></button>
+                        <button className="sortButton" onClick={() => setSortOrder('modelDescending')}><span role="img" aria-label="Guitar">🎸</span><span role="img" aria-label="Index Pointing Down">👇</span></button>
+                    </div>
+                </div>
+                <div className="guitar-container">
+                    <div className="cards">
+                        {guitarData.sort(sortModelAscending).filter(f => (
+                            (f.model.toLowerCase().includes(model.toLowerCase()) || model === '')
+                            && (f.color.toLowerCase().includes(color.toLowerCase()) || color === '')
+                            && (parseFloat(f.price.replace(/[$,]+/g, "")) > minPrice || minPrice === '')
+                            && (parseFloat(f.price.replace(/[$,]+/g, "")) < maxPrice || maxPrice === '')
+                        )
+                        )
+                            .map((data, key) => {
+                                return (
+                                    <div key={key} className="card">
+                                        <img src={'./resizedImages' + data.image.substring(data.image.indexOf('/RAM'), data.image.length - 3) + 'jpg'} alt="guitar" />
+                                        <div className="cardText">
+                                            <div className="model">{data.model}</div>
+                                            <div className="color">{data.color}</div>
+                                            <div className="price">{data.price}</div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    else if (sortOrder === 'modelDescending') {
+        return (
+            <div>
+                <div className="header">
+                    <div className="title"><img src="https://images.ctfassets.net/m8onsx4mm13s/72p8qgeqA4UK7FD2MwyA19/cbc4a71ff92b788b791a53fa683dc6f4/gibson__1_.svg" alt="gibson logo"></img>Modded Mod Shop</div>
+
+                    <div className="centered">
+                        <input id="modelFilter" placeholder="Model" name="filter" type="text" value={model} onChange={event => setModel(event.target.value)}></input>
+                        <input id="colorFilter" placeholder="Color" name="filter" type="text" value={color} onChange={event => setColor(event.target.value)}></input>
+                        <input id="minPriceFilter" placeholder="Minimum Price" name="filter" type="text" value={minPrice} onChange={event => setMinPrice(event.target.value)}></input>
+                        <input id="maxPriceFilter" placeholder="Maximum Price" name="filter" type="text" value={maxPrice} onChange={event => setMaxPrice(event.target.value)}></input>
+                    </div>
+                    <div className="centered">
+                        <button className="sortButton" onClick={() => setSortOrder('priceAscending')}><span role="img" aria-label="Money Bag">💰</span><span role="img" aria-label="Index Pointing Up">👆</span></button>
+                        <button className="sortButton" onClick={() => setSortOrder('priceDescending')}><span role="img" aria-label="Money Bag">💰</span><span role="img" aria-label="Index Pointing Down">👇</span></button>
+                        <button className="sortButton" onClick={() => setSortOrder('modelAscending')}><span role="img" aria-label="Guitar">🎸</span><span role="img" aria-label="Index Pointing Up">👆</span></button>
+                        <button className="sortButton activeButton" onClick={() => setSortOrder('modelDescending')}><span role="img" aria-label="Guitar">🎸</span><span role="img" aria-label="Index Pointing Down">👇</span></button>
+                    </div>
+                </div>
+                <div className="guitar-container">
+                    <div className="cards">
+                        {guitarData.sort(sortModelDescending).filter(f => (
+                            (f.model.toLowerCase().includes(model.toLowerCase()) || model === '')
+                            && (f.color.toLowerCase().includes(color.toLowerCase()) || color === '')
+                            && (parseFloat(f.price.replace(/[$,]+/g, "")) > minPrice || minPrice === '')
+                            && (parseFloat(f.price.replace(/[$,]+/g, "")) < maxPrice || maxPrice === '')
+                        )
+                        )
+                            .map((data, key) => {
+                                return (
+                                    <div key={key} className="card">
+                                        <img src={'./resizedImages' + data.image.substring(data.image.indexOf('/RAM'), data.image.length - 3) + 'jpg'} alt="guitar" />
+                                        <div className="cardText">
+                                            <div className="model">{data.model}</div>
+                                            <div className="color">{data.color}</div>
+                                            <div className="price">{data.price}</div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
